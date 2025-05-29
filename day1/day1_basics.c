840b6650b3d19eef8e16fec64467d45323b62501
kirinryu.dev <kirinryu.dev@gmail.com>
2025-05-29 13:54:12 +0000

    playing withj pointers and references with int data type

--- day1/day1_basics.c
+++ day1/day1_basics.c
@@ -1,10 +1,23 @@
 // this is the a program that will return result of memory allocation
 
-#include <stdioh> 
+#include <stdio.h> 
 #include <stdlib.h>
 
 int main () {
     int *num ;
+    num = (int *)malloc(sizeof(int)) ;
+
+    if (num == NULL ) {
+        printf("Allocation failled. \n");
+        return 1 ;
+    }
+
+    *num = 9 ;
+    printf("Value of num : %d " , *num ) ;
+
+    free(num) ;
+
+    num = NULL ;
 
 
     //return integer
